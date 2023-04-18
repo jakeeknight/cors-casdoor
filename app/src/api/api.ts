eebca40fe26c2bb6703b6fbe42d9b504dc3745bb
Jake Knight <jakeknightweb@gmail.com>
2023-04-18 16:48:35 +0100

    Use casdoor signup url

--- app/src/api/api.ts
+++ app/src/api/api.ts
@@ -1,19 +1,19 @@
-import { IAuthForm } from "@/types/auth"
-import { apiRequest } from "./http"
-import { IApi } from "./interfaces"
-import { casdoor } from "@/services/casdoor"
+import { IAuthForm } from "@/types/auth";
+import { apiRequest } from "./http";
+import { IApi } from "./interfaces";
+import { casdoor } from "@/services/casdoor";
 
 export const api: IApi = {
   health: {
     async check() {
-      const response = await apiRequest("GET", "health")
-      return response
+      const response = await apiRequest("GET", "health");
+      return response;
     },
   },
   auth: {
     async callback(token: string) {
-      const response = await apiRequest("GET", "/auth/callback", null, token)
-      return response
+      const response = await apiRequest("GET", "/auth/callback", null, token);
+      return response;
     },
     async login(data: IAuthForm) {
       const response = await apiRequest(
@@ -22,8 +22,8 @@ export const api: IApi = {
         data,
         null,
         casdoor.getSigninUrl()
-      )
-      return response
+      );
+      return response;
     },
     async register(data: IAuthForm) {
       const response = await apiRequest(
@@ -31,21 +31,21 @@ export const api: IApi = {
         "",
         data,
         null,
-        "http://localhost:8000/api/signup"
-      )
-      return response
+        casdoor.getSignupUrl()
+      );
+      return response;
     },
     async logout() {
-      const response = await apiRequest("POST", "/api/auth/logout")
-      return response
+      const response = await apiRequest("POST", "/api/auth/logout");
+      return response;
     },
     async resetPassword(email: string) {
       const response = await apiRequest(
         "POST",
         "/api/auth/reset-password",
         email
-      )
-      return response
+      );
+      return response;
     },
   },
-}
+};
